1238b4d28e63f5d767823705cfa64cdb1e5ab5fc
Uwihanganyeobed <uwihanganyeobed@gmail.com>
2026-08-21 11:28:33 +0200

    all

--- add.php
+++ add.php
@@ -11,3 +11,28 @@ if($_SERVER["REQUEST_METHOD"]== "POST"){
     header("Location: index.php");
 }
 ?>
+
+<!DOCTYPE html>
+<html >
+<head>
+    <title>Add Posts</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Add Post</h1>
+        <form action=""method="POST">
+            <label>Author</label>
+            <input type="text"
+            name="author"
+            required>
+            <label>Body</label>
+            <input type="text"
+            name="body"
+            required>
+            <button type="submit">Add Post</button>
+        </form>
+        <a href="index.php">Back Home</a>
+    </div>
+</body>
+</html>
